678043f897755488b082e087f330e128a7477218
maxengelhard <maxvengelhard@gmail.com>
2024-06-05 08:02:00 -0500

    Adding in print secret

--- webhook.py
+++ webhook.py
@@ -3,27 +3,31 @@ import hmac
 import hashlib
 import os
 import subprocess
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-GITHUB_SECRET = 'your_github_secret_here'
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    load_dotenv()
+    GITHUB_SECRET = os.getenv('github_secret')
+    print(request)
     if request.method == 'POST':
-        print(request.method)
-        # # Verify the request signature
-        # signature = request.headers.get('X-Hub-Signature-256')
-        # if signature is None:
-        #     abort(403)
         
-        # sha_name, signature = signature.split('=')
-        # if sha_name != 'sha256':
-        #     abort(403)
+        # Verify the request signature
+        signature = request.headers.get('X-Hub-Signature-256')
+        if signature is None:
+            abort(403)
         
-        # mac = hmac.new(bytes(GITHUB_SECRET, 'utf-8'), msg=request.data, digestmod=hashlib.sha256)
-        # if not hmac.compare_digest(mac.hexdigest(), signature):
-        #     abort(403)
+        sha_name, signature = signature.split('=')
+        if sha_name != 'sha256':
+            abort(403)
+        
+        mac = hmac.new(bytes(GITHUB_SECRET, 'utf-8'), msg=request.data, digestmod=hashlib.sha256)
+        if not hmac.compare_digest(mac.hexdigest(), signature):
+            abort(403)
 
         # # Pull the latest changes from GitHub
         # subprocess.call(['git', 'pull'])
